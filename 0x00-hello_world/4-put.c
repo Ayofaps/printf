0e496249c47b295c6c9a0769219db63479e6780f
Ayofaps <ayofaps@yahoo.com>
2022-12-08 08:26:30 -0800

    4-put.c

---
+++ 0x00-hello_world/4-put.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+/**
+ * main - prints thr phrase "Programming is like building a multiligual puzzule
+ * Return: 0 if executed properly, non zero otherwise
+ */
+int main (void)
+{
+	put("\"Programming is like building a multilingual puzzule");
+	return (0);
+
+}
