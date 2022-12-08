0470514d2709d2a0be49fbbe5ca3226db55f2bfb
Ayofaps <ayofaps@yahoo.com>
2022-12-08 09:01:11 -0800

    4 puts

--- 0x00-hello_world/4-put.c
+++ 0x00-hello_world/4-put.c
@@ -1,12 +1,11 @@
-#!/bin/bash
-
-/**
- * main - prints thr phrase "Programming is like building a multiligual puzzule
- * Return: 0 if executed properly, non zero otherwise
+#include <stdio.h>
+/*
+  main - prints thr phrase "Programming is like building a multiligual puzzule
+  Return: 0 if executed properly, non zero otherwise
  */
 int main (void)
 {
 	put("\"Programming is like building a multilingual puzzule");
 	return (0);
-
 }
+
