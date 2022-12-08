d41ecd5a249fe38b6b16f4f0ed7706dd0ef7dd48
Ayofaps <ayofaps@yahoo.com>
2022-12-08 09:58:21 -0800

    4 puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,15 @@
 
-#include <string.h>
+#include <stdio.h>
 
 /**
       * main - Main function of the project
       *
       *
       * Return: return 0 all the time 
-     */
+* /
+int main(void)
+ {
+   put ("\"Programming is like building a multilingual puzzle");
+   return (0);
+ }
+
