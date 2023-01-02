0e074d959413556ea50ab7d0dfbbbff1a889fb2f
Ayofaps <ayofaps@yahoo.com>
2023-01-02 13:38:05 -0800

    1 memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,9 +8,9 @@
  * Return: Always 0 (Sucess)
  */
 
-char *_memcpy(char *dest, char *src, unsigned in n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned in i;
+	unsigned int i;
 
 	for (i = 0; i < n; i++)
 		dest[i] = src[i];
