1b75c4a76278b32f7501e72067b5c30c740ad2b5
Ayofaps <ayofaps@yahoo.com>
2023-01-02 16:52:18 -0800

    100 set spring

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -2,9 +2,10 @@
 
 /**
  * set_string - sets the value of a pointer to a char
- * @s: content is to be copied
- * @to: string
- * Return: void
+ * @s: pointer to pointer
+ * @to: pointer to char
+ *
+ * Return: void.
  */
 
 void set_string(char **s, char *to)
